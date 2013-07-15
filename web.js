1993461c2a34f7364a60e42a6aa3b7ffe850cf62
robbaran <robbaranowski@gmail.com>
2013-07-15 04:11:37 +0000

    new fiel to read from

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var buf = fs.readFileSync('index.html");;
+var buf = fs.readFileSync("bs.html");
 
 app.get('/', function(request, response) {
   response.send(buf.toString);
